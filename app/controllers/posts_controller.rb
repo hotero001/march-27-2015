9c141990760d51629a1e2a5e0b868bd2274f5680
Hector Otero <otero111@hotmail.com>
2015-03-28 21:57:57 -0700

    added edit buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,17 +18,23 @@ class PostsController < ApplicationController
   		flash[:notice] = "You have successfully created a new post"
   		redirect_to posts_path
   	else
-  		
+
   		render :new
   	end
   end
 
   def edit
-
+  	@post = Post.find(params[:id])
   end
 
   def update
-
+  	@post = Post.find(params[:id])
+  	if @post.update(post_params)
+  		flash[:notice] = "The post has been updated successfully"
+  		redirect_to post_path(@post)
+  	else
+  		render :edit
+  	end
   end
 
   private
